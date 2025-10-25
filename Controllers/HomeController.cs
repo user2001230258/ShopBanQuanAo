e482f0e413e2a233d014378a766db7dbc3a476bd
Hoang Van Hoan <2001230258@hufi.edu.vn>
2025-10-25 18:40:45 +0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webmvc.Models;
+using System.Data.SqlClient;
+
+namespace webmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        DO_AN_GIOVANNIEntities DATA = new DO_AN_GIOVANNIEntities();
+        public ActionResult Index()
+        {
+            List<SANPHAM> DSSP = DATA.SANPHAMs.ToList();
+            return View(DSSP);
+        }
+        public ActionResult MENU_chude()
+        {
+            List<DANHMUC> dm = DATA.DANHMUCs.ToList();
+            return View(dm);
+        }
+    }
+}
