ba10d0f73116765fc606c4a3da801e215547a63d
Hoang Van Hoan <2001230258@hufi.edu.vn>
2025-10-25 18:41:31 +0700

    Add files via upload

---
+++ Models/SP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webmvc.Models
+{
+    public class SP{
+        public string MaSP { get; set; }
+        public string TenSP { get; set; }
+        public decimal GiaGoc { get; set; }
+        public decimal GiaGiam { get; set; }
+        public string MoTa { get; set; }
+        public string TenTH { get; set; }
+        public string TenDM { get; set; }
+    }
+}
